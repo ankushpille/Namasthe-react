1f772b910f34f1300480d1d97dc510dbb892e4a2
ankushpille <ankush.pille1@gmail.com>
2025-09-20 16:06:58 +0530

    nested childs in react

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
-const heading = React.createElement("h1", {}, "Hello world from inside div")
-const parent1 = React.createElement("div",{},heading);
-const parent =  React.createElement("div",{},parent1);
+const heading = React.createElement("h1", {id:"heading"}, "Hello world from inside div")
+const heading1 = React.createElement("h2",{id:"heading2"},"Hello world from h2 tag")
+const parent1 = React.createElement("div",{id:"child"},[heading,heading1]);
+const heading0 = React.createElement("h1", {id:"heading0"}, "Hello world from inside div")
+const heading2 = React.createElement("h2",{id:"heading3"},"Hello world from h2 tag")
+const parent2 = React.createElement("div",{id:"child2"},[heading,heading1]);
+const parent =  React.createElement("div",{id:"parent"},[parent1,parent2]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(parent);
