c0bb979c2f36e4301556b6249b3e1f5414ea392c
ankushpille <ankush.pille1@gmail.com>
2025-09-30 10:18:48 +0530

    using jsx consoled heading

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 
 
-const heading = React.createElement("h1", {id:"heading"}, "Hello world from inside div")
-const heading1 = React.createElement("h2",{id:"heading2"},"Hello world from h2 tag")
-const parent1 = React.createElement("div",{id:"child"},[heading,heading1]);
-const heading0 = React.createElement("h1", {id:"heading0"}, "Hello world from inside div")
-const heading2 = React.createElement("h2",{id:"heading3"},"Hello world from h2 tag")
-const parent2 = React.createElement("div",{id:"child2"},[heading0,heading2]);
-const parent =  React.createElement("div",{id:"parent"},[parent1,parent2]);
+const heading = React.createElement('h1',{id: 'h1'},'hello this heading from react')
+console.log("heading",heading);
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent);
+
+const reactHeading = <h1>Hello ankush from jsx</h1>
+
+console.log("reactHeading",reactHeading);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(reactHeading);
 
