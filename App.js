2dedf0773e9eca4317453473bbb1278219af011b
ankushpille <ankush.pille1@gmail.com>
2025-09-30 10:38:03 +0530

    commented how jsx is converted in to js engine understable way

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 
 
+//react.createelement => js-object => htmlelement(render)
 const heading = React.createElement('h1',{id: 'h1'},'hello this heading from react')
 console.log("heading",heading);
 
 
+//jsx => React.createelement => js-object => htmlelement(render)
 const reactHeading = <h1>Hello ankush from jsx</h1>
 
 console.log("reactHeading",reactHeading);
